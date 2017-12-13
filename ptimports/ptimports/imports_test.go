afe8e0225436071ccf068e59d23c3ebede1f3f31
Nick Miyake <nmiyake@palantir.com>
2017-12-13 12:44:51 -0800

    Add unit tests for ptimports

---
+++ ptimports/ptimports/imports_test.go
@@ -0,0 +1,68 @@
+// Copyright 2016 Palantir Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ptimports_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/palantir/checks/ptimports/ptimports"
+)
+
+func TestPtImports(t *testing.T) {
+	for i, tc := range []struct {
+		name string
+		in   string
+		want string
+	}{
+		{
+			"Groups imports based on builtin, external, and project-local",
+			`package foo
+
+import "github.com/palantir/checks/ptimports/ptimports"
+import "bytes"
+import "golang.org/x/tools/imports"
+
+func Foo() {
+	_ = bytes.Buffer{}
+	_ = ptimports.Process
+	_ = imports.Process
+}
+`,
+			`package foo
+
+import (
+	"bytes"
+
+	"golang.org/x/tools/imports"
+
+	"github.com/palantir/checks/ptimports/ptimports"
+)
+
+func Foo() {
+	_ = bytes.Buffer{}
+	_ = ptimports.Process
+	_ = imports.Process
+}
+`,
+		},
+	} {
+		got, err := ptimports.Process("test.go", []byte(tc.in))
+		require.NoError(t, err, "Case %d: %s", i, tc.name)
+		assert.Equal(t, tc.want, string(got), "Case %d: %s", i, tc.name)
+	}
+}
